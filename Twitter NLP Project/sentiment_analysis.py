ff8a9c7d3aa88a15c2c1ab451c2fc6f2ed87e19e
zb8787 <54961500+zb8787@users.noreply.github.com>
2020-10-05 19:59:56 -0400

    Add files via upload

---
+++ Twitter NLP Project/sentiment_analysis.py
@@ -0,0 +1,46 @@
+import argparse
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+def analyze(the_filename):
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language.LanguageServiceClient()
+
+    with open(the_filename, "r", encoding="utf8") as the_file:
+        # Instantiates a plain text document.
+        content = the_file.read()
+
+    document = types.Document(content=content, type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+
+    # Print the results
+    print_result(annotations)
+	
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print(
+            "Sentence {} has a sentiment score of {}".format(index, sentence_sentiment)
+        )
+
+    print(
+        "Overall Sentiment: score of {} with magnitude of {}".format(score, magnitude)
+    )
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "the_filename",
+        help="The filename of the tweet you'd like to analyze.",
+    )
+    args = parser.parse_args()
+
+    analyze(args.the_filename)
